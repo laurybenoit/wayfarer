48c040db4cbf4f7e695b41d67a9423148caf5aa7
diaba <hwar@cincinnatistate.edu>
2022-01-20 17:52:55 -0500

    Setting up the wayfarer project

---
+++ wayfarer/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CityComponent } from './city/city.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+const routes: Routes = [
+  {
+    
+    path: '',
+    component: NavbarComponent,
+    
+  },
+  {
+    path:'city',
+    component: CityComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ wayfarer/src/app/city/cities.ts
@@ -0,0 +1,27 @@
+export const CITIES = [
+  {
+    city: 'London',
+    image:
+      'https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    posts: ['This city is ancient', 'Love fish & chips'],
+  },
+
+  {
+    city: 'Sydney',
+    image:
+      'https://images.pexels.com/photos/995765/pexels-photo-995765.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    posts: ['Seafood is great'],
+  },
+
+  {
+    city: 'San Francisco',
+    image:
+      'https://images.pexels.com/photos/208745/pexels-photo-208745.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+  },
+
+  {
+    city: 'Seattle',
+    image:
+      'https://images.pexels.com/photos/336512/pexels-photo-336512.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+];
