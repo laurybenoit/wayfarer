d29c7f2821a622cb1618517436fdc8778fc5c1d7
MichelleLucero <iammichellelucero@gmail.com>
2022-01-21 12:41:05 -0500

    Habsatou/Laury/Michelle displays cities without styling

--- wayfarer/src/app/app.component.ts
+++ wayfarer/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { CITIES } from './city/cities';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wayfarer';
+  cities = CITIES;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {}
 }
--- wayfarer/src/app/city/city.component.ts
+++ wayfarer/src/app/city/city.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { CITIES } from './cities';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-city',
   templateUrl: './city.component.html',
-  styleUrls: ['./city.component.css']
+  styleUrls: ['./city.component.css'],
 })
 export class CityComponent implements OnInit {
-  city:any;
+  cities = CITIES;
+  city: any;
+  cityIndex: string | null = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((param) => {
+      // deal with the id parameter
+      console.log(param.get('id'));
+      this.cityIndex = param.get('id');
+      // find by index
+      this.city = CITIES.find((j) => {
+        const paramId: string = param.get('id') || '';
+        return j.id === parseInt(paramId);
+      });
+    });
   }
-
 }
